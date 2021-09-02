9f97aa457a9e4132730cff20b1ea03054ece0402
e-Henderson <eddie.henderson@revature.net>
2021-09-02 11:46:36 -0400

    1st angular commits for category

--- WebInventory/src/app/app.module.ts
+++ WebInventory/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes=[
+  {path: '', component: CategoryComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ WebInventory/src/app/category/category.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Data, Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent implements OnInit {
+
+  title='Inventory Categories'
+  data:any=[];//might change this to category or something to reflect that
+  selectedData?: Data;
+
+  public _url="";//place url here to test
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) { }
+
+  fetchCategories(){
+    this.http.get(this._url)
+    .subscribe(data=>{
+      console.log(data);
+      this.data=data;
+    },error=>console.error(error));
+  }
+
+  ngOnInit(): void {
+    this.fetchCategories();
+  }
+
+  onSelect(data: Data): void{
+    this.selectedData=data;
+  }
+
+}
