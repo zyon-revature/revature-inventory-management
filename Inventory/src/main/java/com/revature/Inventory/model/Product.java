b4bebb2bb520a6b3e0a711d41091d01cf87e6753
e-Henderson <eddie.henderson@revature.net>
2021-09-04 15:17:13 -0400

    7th category commit
    removed other extra code

--- Inventory/src/main/java/com/revature/Inventory/model/Product.java
+++ Inventory/src/main/java/com/revature/Inventory/model/Product.java
@@ -1,18 +1,21 @@
 package com.revature.Inventory.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+import lombok.*;
 
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity (name="product")
 public class Product {
+	
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_generator")
     @SequenceGenerator(name="id_generator", sequenceName = "product_id_seq", allocationSize = 1)
-	
 	private int id;
 	@Column
 	private String title;
@@ -22,69 +25,5 @@ public class Product {
 	private String manufacturer;
 	@Column
 	private int minlimit;
-	
-	public Product() {
-		super();
-	}
-	
-	public Product(String category) {
-		super();
-		this.category=category;
-	}
-	
-	public Product(int id,String title, String category, String manufacturer, int minlimit) {
-		super();
-		
-		this.id=id;
-		this.title=title;
-		this.category=category;
-		this.manufacturer=manufacturer;
-		this.minlimit=minlimit;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public String getManufacturer() {
-		return manufacturer;
-	}
-
-	public void setManufacturer(String manufacturer) {
-		this.manufacturer = manufacturer;
-	}
-
-	public int getMinlimit() {
-		return minlimit;
-	}
-
-	public void setMinlimit(int minlimit) {
-		this.minlimit = minlimit;
-	}
-	
-	@Override
-	public String toString() {
-		return "["+this.category+"]";
-	}
 	
 }
