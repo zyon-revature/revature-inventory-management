48cef1930a2ac8ee21772a5ee68618ca0bbc1b21
ChrisWright12 <86730778+ChrisWright12@users.noreply.github.com>
2021-09-04 14:30:51 -0500

    Invoice Code(Still Broken)

---
+++ Inventory/src/main/java/com/revature/Inventory/manager/ProductStockManager.java
@@ -0,0 +1,23 @@
+package com.revature.Inventory.manager;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.revature.Inventory.model.ProductStock;
+import com.revature.Inventory.model.Threshold;
+
+public interface ProductStockManager {
+	
+    List<ProductStock> findAll();
+	
+    ProductStock findById(int id);
+	
+    ProductStock create(ProductStock ps);
+
+    ProductStock update(@Valid ProductStock ps);
+
+	List<Threshold> findAllThreshold();
+	
+	List<Threshold> findThreshold();
+}
---
+++ Inventory/src/main/java/com/revature/Inventory/model/ProductStock.java
@@ -0,0 +1,153 @@
+package com.revature.Inventory.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity (name="productstock")
+public class ProductStock{
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_generator")
+	@SequenceGenerator(name = "id_generator", sequenceName = "productstock_id_seq", allocationSize = 1)
+	private int id;
+	@JoinColumn(name = "productid", nullable = false)
+	private Product productId;
+	@Column
+	private Date transactiondate;
+	@Column
+	private String vendor;
+	@Column
+	private String batchcode;
+	@Column
+	private String invoicenumber;
+	@Column
+	private int quantity;
+	@Column
+	private String transactiontype;
+
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "productid")
+	private Product product;
+	
+	public ProductStock(int id, String vendor, String batchCode,String invoiceNumber,
+		int quantity, String transactionType) {
+		this.batchcode = batchCode;
+		this.invoicenumber = invoiceNumber;
+		this.productId.setId(id);
+		this.quantity = quantity;
+		this.transactiontype = transactionType;
+		this.vendor = vendor;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ProductStock [id=" + id + ", productId=" + productId + ", transactionDate=" + transactiondate
+				+ ", vendor=" + vendor + ", batchCode=" + batchcode + ", invoiceNumber=" + invoicenumber + ", quantity="
+				+ quantity + ", transactionType=" + transactiontype + "]";
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public Product getProductId() {
+		return productId;
+	}
+
+
+	public void setProductId(Product productId) {
+		this.productId = productId;
+	}
+
+
+	public Date getTransactiondate() {
+		return transactiondate;
+	}
+
+
+	public void setTransactiondate(Date transactiondate) {
+		this.transactiondate = transactiondate;
+	}
+
+
+	public String getVendor() {
+		return vendor;
+	}
+
+
+	public void setVendor(String vendor) {
+		this.vendor = vendor;
+	}
+
+
+	public String getBatchcode() {
+		return batchcode;
+	}
+
+
+	public void setBatchcode(String batchcode) {
+		this.batchcode = batchcode;
+	}
+
+
+	public String getInvoicenumber() {
+		return invoicenumber;
+	}
+
+
+	public void setInvoicenumber(String invoicenumber) {
+		this.invoicenumber = invoicenumber;
+	}
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+	public String getTransactiontype() {
+		return transactiontype;
+	}
+
+
+	public void setTransactiontype(String transactiontype) {
+		this.transactiontype = transactiontype;
+	}
+
+
+	public Product getProduct() {
+		return product;
+	}
+
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+
+
+
+}
