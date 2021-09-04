d56fc4fcd132899e70b78c20ab21ba0f8a088154
e-Henderson <eddie.henderson@revature.net>
2021-09-04 11:26:43 -0400

    4th angular commit
    Continuing to test same method from last night and connecting /{category} url

--- WebInventory/src/app/app.component.ts
+++ WebInventory/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,17 +7,6 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  websiteList: any =['ItSolutionStuff.com','HDTuto.com','Nicesnippets.com'];
-  form = new FormGroup({
-    website: new FormControl('', Validators.required)
-  });
 
-  get f(){
-    return this.form.controls;
-  }
-
-  submit(){
-    console.log(this.form.value);
-  }
   title = 'WebInventory';
 }
--- WebInventory/src/app/category/category.component.ts
+++ WebInventory/src/app/category/category.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Data, Router } from '@angular/router';
 import { Category } from '../category';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -15,7 +16,7 @@ export class CategoryComponent implements OnInit {
   data: any=[];//change this to category
   selectedData?: Data;
   
-  categories: Category[]=[];
+  categories: Category[]=[];//could use this in place of the websiteList
   category: Category={
     id: 0,
     title: '',
@@ -24,9 +25,24 @@ export class CategoryComponent implements OnInit {
     category: ''
   }
   selectedCategory?: Category;
-  
+  //test//
+  categoryList: any=['Electronics','Appliances','Home Decor'];
+
+  form = new FormGroup({
+    website: new FormControl('',[Validators.required])
+  });
+
+  get f(){
+    return this.form.controls;
+  }
+
+  submit(){
+    console.log(this.form.value);
+  }
+  //end test//
 
   public _url="http://localhost:8080/categories";//place url here to test
+  //above url should be "http://localhost:8080/categories/{category}",where the value is set to {category} and those are returned
   constructor(
     private router: Router,
     private http: HttpClient
